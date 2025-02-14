895a6f8a397d60dccaf4df5591f4b812505d4b6c
RajputRohit26 <mrraaj083@gmail.com>
2025-02-14 10:17:33 +0530

    14-02-2025

---
+++ Variable.java
@@ -0,0 +1,43 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Variable {
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter A :");
+		a=sc.nextInt();
+		System.out.print("Enter B :");
+        b=sc.nextInt();
+        c=a+b;
+		System.out.println("Addition : "+c);
+		
+		System.out.print("Enter A :");
+		a=sc.nextInt();
+		System.out.print("Enter B :");
+		b=sc.nextInt();
+		c=a-b;
+		System.out.println("Substraction :"+c);
+		
+		System.out.print("Enter A :");
+		a=sc.nextInt();
+		System.out.print("Enter B :");
+		b=sc.nextInt();
+		c=a/b;
+		System.out.println("Division :"+c);
+		
+		System.out.print("Enter A :");
+		a=sc.nextInt();
+		System.out.print("Enter B :");
+		b=sc.nextInt();
+		c=a*b;
+		System.out.println("Multiplication :"+c);
+		
+		
+		
+		
+		
+	}
+
+}

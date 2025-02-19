cbc8eaa740de8c79dbfccd559408e2f81a282d5f
RajputRohit26 <mrraaj083@gmail.com>
2025-02-19 10:19:04 +0530

    19-02-2025

---
+++ Whiledemo.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Whiledemo {
+	public static void main(String[] args) {
+		int n,sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter N :");
+		n=sc.nextInt();
+		do
+		{
+			sum=sum+n;
+			n--;
+			
+		}while(n>=0);
+		System.out.println("Sum :"+sum);
+	}
+
+}
